0a89d0a2312b90d4ae188932743977806496c162
David Wright <dave@coderstudios.com>
2019-06-06 18:58:28 +0100

    Add Laravel Cashier package and integrations to stock Laravel

--- webroot/database/migrations/2014_10_12_000000_create_users_table.php
+++ webroot/database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,22 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->string('stripe_id')->nullable()->collation('utf8mb4_bin');
+            $table->string('card_brand')->nullable();
+            $table->string('card_last_four', 4)->nullable();
+            $table->timestamp('trial_ends_at')->nullable();
+        });
+
+        Schema::create('subscriptions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->string('name');
+            $table->string('stripe_id')->collation('utf8mb4_bin');
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamp('trial_ends_at')->nullable();
+            $table->timestamp('ends_at')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -32,5 +48,6 @@ class CreateUsersTable extends Migration
     public function down()
     {
         Schema::dropIfExists('users');
+        Schema::dropIfExists('subscriptions');
     }
 }
